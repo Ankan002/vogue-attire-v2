2129eca296c0ad729c97c8c1770d84b1229b99f8
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-12-03 00:25:55 +0530

    chore: :lipstick: add font styles

--- apps/admin-web/app/page.tsx
+++ apps/admin-web/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-center bg-white text-black text-4xl">
+		<main className="flex min-h-screen flex-col items-center justify-center bg-white text-black text-xl font-oxanium">
 			{process.env["NAME"]} DEFAULT PAGE
 		</main>
 	);
--- packages/tailwind-config/tailwind-config.ts
+++ packages/tailwind-config/tailwind-config.ts
@@ -8,7 +8,17 @@ export const tailwindConfig: Config = {
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				"geist-sans": ["var(--font-geist-sans)"],
+				"geist-mono": ["var(--font-geist-mono)"],
+				quicksand: "'Quicksand', sans-serif",
+				manrope: "'Manrope', sans-serif",
+				"fira-code": "'Fira Code', monospace",
+				muli: "'muli', sans-serif",
+				oxanium: "'Oxanium', cursive",
+			},
+		},
 	},
 	plugins: [],
 };
