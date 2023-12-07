831180c12b74547de064b487c02e34fb8c6aeba6
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-12-07 15:18:08 +0530

    chore: :hammer: update .env example

--- apps/admin-web/app/layout.tsx
+++ apps/admin-web/app/layout.tsx
@@ -5,39 +5,17 @@ import "./globals.css";
 import { RecoilProvider, ReactQueryProvider } from "common-providers";
 import { ThemeProvider } from "@/components/providers";
 import { ThemeToggleBtn } from "@/components/common";
-import { getDbInstance } from "@/config";
 
 export const metadata: Metadata = {
 	title: "Vogue Attire Admin",
 	description: "The Protected ADMIN CMS for Vogue Attire",
 };
 
-export default async function RootLayout({
+export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const db = getDbInstance();
-
-	const startTime = Date.now();
-
-	const admin = await db.admin.findUnique({
-		where: {
-			email: "ankanbhattacharya89@gmail.com",
-		},
-		select: {
-			id: true,
-			email: true,
-			name: true,
-		},
-	});
-
-	const endTime = Date.now();
-	
-	console.log("Time taken:", (endTime - startTime), "ms");
-
-	console.log(admin);
-
 	return (
 		<html lang="en">
 			<body className={`${GeistSans.variable} ${GeistMono.variable}`}>
