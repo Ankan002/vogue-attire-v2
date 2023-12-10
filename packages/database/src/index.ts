321b90797c52f1a1187044e9820225d57a32c0a2
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-12-10 12:51:58 +0530

    fix: :ambulance: update db exports

--- packages/database/src/index.ts
+++ packages/database/src/index.ts
@@ -1,4 +1,18 @@
 export { PrismaClient } from "@prisma/client";
 export { PrismaLibSQL } from "@prisma/adapter-libsql";
 
-export * from "./turso-client";
+export type {
+	Address,
+	Admin,
+	Attribute,
+	CartItem,
+	Category,
+	Order,
+	OrderItem,
+	OrderItemTax,
+	OrderStatus,
+	PaymentStatus,
+} from "@prisma/client";
+
+// !Note: If you want to use turso or libsql with this project. Please go to turso-client and turn on the options and then turn on the commented line below. Then update the above code file as per requirements.
+// export * from "./turso-client";
