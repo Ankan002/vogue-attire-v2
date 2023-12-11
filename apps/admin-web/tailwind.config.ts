0d4b71e9d0b4ffb4c8aee8e81bb40d1d12707cfc
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-12-11 23:42:55 +0530

    chore: :lipstick: fix tailwind issue

--- apps/admin-web/tailwind.config.ts
+++ apps/admin-web/tailwind.config.ts
@@ -1,5 +1,5 @@
-import type { Config } from "tailwindcss";
 import { tailwindConfig } from "tailwind-config";
+import { Config } from "tailwindcss";
 
 const config: Config = {
 	...tailwindConfig,
@@ -7,6 +7,8 @@ const config: Config = {
 		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
 		"./components/**/*.{js,ts,jsx,tsx,mdx}",
 		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"../../packages/brand-kit/**/*.{js,ts,jsx,tsx,mdx}",
 	],
 };
+
 export default config;
