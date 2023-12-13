9edc4635bdbff48e844d4c9e2ced90df9ae3db30
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2023-12-13 23:50:52 +0530

    feat: :sparkles: add ui

--- packages/tailwind-config/tailwind-config.ts
+++ packages/tailwind-config/tailwind-config.ts
@@ -3,12 +3,34 @@ import { Config } from "tailwindcss";
 export const tailwindConfig: Config = {
 	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
-		"./components/**/*.{js,ts,jsx,tsx,mdx}",
-		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+		"./pages/**/*.{ts,tsx}",
+		"./components/**/*.{ts,tsx}",
+		"./app/**/*.{ts,tsx}",
+		"./src/**/*.{ts,tsx}",
 	],
 	theme: {
+		container: {
+			center: true,
+			padding: "2rem",
+			screens: {
+				"2xl": "1400px",
+			},
+		},
 		extend: {
+			keyframes: {
+				"accordion-down": {
+					from: { height: "0" },
+					to: { height: "var(--radix-accordion-content-height)" },
+				},
+				"accordion-up": {
+					from: { height: "var(--radix-accordion-content-height)" },
+					to: { height: "0" },
+				},
+			},
+			animation: {
+				"accordion-down": "accordion-down 0.2s ease-out",
+				"accordion-up": "accordion-up 0.2s ease-out",
+			},
 			fontFamily: {
 				"geist-sans": ["var(--font-geist-sans)"],
 				"geist-mono": ["var(--font-geist-mono)"],
@@ -43,5 +65,5 @@ export const tailwindConfig: Config = {
 			},
 		},
 	},
-	plugins: [],
+	plugins: [require("tailwindcss-animate")],
 };
