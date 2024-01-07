3e47072b489d0768effb52dac0c1859fa7e6cc76
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-01-07 12:48:05 +0530

    feat: :sparkles: add get current pathname

--- apps/admin-web/components/common/sidebar/hook.ts
+++ apps/admin-web/components/common/sidebar/hook.ts
@@ -1,10 +1,14 @@
 import { menuActiveAtom } from "@/atoms";
 import { useRecoilValue } from "recoil";
+import { usePathname } from "next/navigation";
 
 export const useSidebar = () => {
 	const isMenuActive = useRecoilValue<boolean>(menuActiveAtom);
 
+	const currentPathname = usePathname();
+
 	return {
 		isMenuActive,
+		currentPathname,
 	};
 };
--- apps/admin-web/components/home-page/home-page/home-page.tsx
+++ apps/admin-web/components/home-page/home-page/home-page.tsx
@@ -3,11 +3,15 @@
 import { useHomePage } from "./hook";
 
 const HomePage = () => {
-    const { isMenuActive } = useHomePage();
+	const { isMenuActive } = useHomePage();
 
 	return (
-		<div className={`flex min-h-screen flex-col bg-white dark:bg-primary-dark text-primary-dark dark:text-primary-light text-xl font-oxanium pt-12 ${isMenuActive ? "lg:ml-64 md:ml-56 ml-0" : "ml-16"}`}>
-			DEFAULT PAGE
+		<div
+			className={`flex min-h-screen flex-col bg-white dark:bg-primary-dark text-primary-dark dark:text-primary-light text-xl font-oxanium pt-12 ${
+				isMenuActive ? "lg:ml-64 md:ml-56 ml-0" : "ml-16"
+			}`}
+		>
+			<h1 className="px-5 py-2">DEFAULT PAGE</h1>
 		</div>
 	);
 };
