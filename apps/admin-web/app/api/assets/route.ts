268eb9769c3921cd8a14131208967460865f3a31
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2024-01-10 11:24:42 +0530

    feat: :sparkles: add api handler

--- apps/admin-web/app/api/assets/route.ts
+++ apps/admin-web/app/api/assets/route.ts
@@ -2,9 +2,9 @@ import { NextResponse } from "next/server";
 import { APIHandler } from "utils";
 
 export const POST = APIHandler(async (request) => {
-	// if (request.method === "POST") {
-	// 	throw new APIError("BOOM!! MF", 400);
-	// }
+	// const requestFormData = await request.formData();
+
+	// const tag = requestFormData.get("")
 
 	return NextResponse.json(
 		{
